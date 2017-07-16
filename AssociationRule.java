4efc39a68b5c5bb235da33a36c05c91714877559
hmr8 <hmr8@njit.edu>
2017-07-15 22:41:27 -0700

    Create AssociationRule.java

---
+++ AssociationRule.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+public class AssociationRule {
+private Itemset itemsetA;
+private Itemset itemsetB;
+public AssociationRule(Itemset itemsetA, Itemset itemsetB) {
+this.itemsetA = itemsetA;
+this.itemsetB = itemsetB;
+}
+public Itemset getItemsetA() {
+return itemsetA;
+}
+public Itemset getItemsetB() {
+return itemsetB;
+}
+public String toString() {
+return "{" + itemsetA + "} => {" + itemsetB + "}";
+}
+}
